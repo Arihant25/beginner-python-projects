ce6e3feaa599e3e9a674ae3572054b6432aa146e
Arihant Tripathy <arihanttr@live.com>
2020-06-11 10:08:10 +0530

    Create guessing-game.py

---
+++ guessing-game.py
@@ -0,0 +1,20 @@
+import random
+
+tries = 0
+limit = int(input("Till what number do you want me to think of?"))
+number = random.randint(1, limit)
+guess = int(input("I'm thinking of a number from 1 to " + str(limit) + ". What is it?"))
+
+while guess != number:
+    if guess < number:
+        print("Your number was too low...")
+        tries = tries + 1
+    else:
+        print("Your number was too high...")
+        tries = tries + 1
+    guess = int(input("Please try again..."))
+print("Congratulations! It took you " + tries + " to reach the answer!")
+if tries > 1:
+    print("You can do better!")
+else:
+    print("Your luck is shining today!")
