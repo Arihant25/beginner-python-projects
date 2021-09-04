8df5888e6ebd587cebd935cf28231a6a021bab91
Arihant Tripathy <arihanttr@live.com>
2021-09-04 11:18:34 +0530

    Optimize prime.py some more Thanks @this_guy_again for the idea!

--- prime.py
+++ prime.py
@@ -1,7 +1,7 @@
 num = int(input("Enter a number: "))
 is_prime = True
 
-for i in range(2, num // 2 + 1):
+for i in range(2, int(num ** 1 / 2) + 1):
     if num % i == 0:
         is_prime =  False
         break
